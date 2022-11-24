8d98490d420ba9cffaf338c41667cb8728158eea
AbdurRahman-34 <get.abdurahman@gmail.com>
2022-11-24 20:54:40 +0600

    carusel, Homepage update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './Components/Shared/Header/Header';
+import Home from "./Components/HomePage/Home/Home"
+import Signup from './Components/Signup/Signup';
+import Login from './Components/Login/Login';
+import NotFound from './Components/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Header/>
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/login' element={<Login/>}></Route>
+      <Route path='/signup' element={<Signup/>}></Route>
+      <Route path='*' element={<NotFound/>}></Route>
+    </Routes>
     </div>
   );
 }
---
+++ src/Components/Shared/Header/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand to="/">Navbar</Navbar.Brand>
+          <Nav className="nav-container">
+            <Link to="/">Home</Link>
+            <Link to="/signup">Sign Up</Link>
+            <Link to="/login">Login</Link>
+            <Link to="/contact">Contact</Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Header;
